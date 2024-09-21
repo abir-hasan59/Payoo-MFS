5102192f15a874005448fb8fa954bdb83a5b2bb8
Md Abir Hasan <tabir085@gmail.com>
2024-09-21 20:37:00 +0600

    add money operation complete

--- js/home.js
+++ js/home.js
@@ -9,4 +9,23 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     // get input pin number
     const inputPinNumber = document.getElementById('input-pin').value;
     console.log(inputPinNumber);
+
+    // verify pin but wrong way
+    if(inputPinNumber === '1234'){
+        console.log('added money on your account');
+        // get the current balance
+        const balance = document.getElementById('current-balance').innerText;
+        console.log(balance)
+        // add addMoneyInput with Balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        console.log(addMoneyNumber,typeof balanceNumber)
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance)
+        // Update the balance 
+        document.getElementById('current-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money! Please try again.')
+    }
 });
